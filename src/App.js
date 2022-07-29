847fcfef6a40387f7d9451e3eeb368a1d11cd58f
George Olufemi <georgeoluolufemi@gmail.com>
2022-07-29 11:21:32 +0100

    Functional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
 export default function App() {
   const [weather, setWeather] = useState({});
   const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    if(location === "") {
+      setWeather({})
+    }
+  }, [weather, location])
 
   const processFunc = (url, apiKey, params) => {
     if (url){
